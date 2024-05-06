0a95f34fef9ffe26687d9c8c8f2e81ee26d6bc72
Shivesh Nandan <kshivesh929@gmail.com>
2024-05-06 17:12:51 +0530

    7

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,20 +3,20 @@ import hi from 'next/font/local'
 import Navbar from './navbar'
 import HeroSection from './heroSection'
 
-const YourFont = hi({
-  src:'../public/font/HelveticaNowDisplay-light.woff',
-  display: 'swap',
-})
+// const YourFont = hi({
+//   src:'../public/font/HelveticaNowDisplay-light.woff',
+//   display: 'swap',
+// })
 
 export default function Home() {
   return (
     <>
-    <div className={YourFont.className}>
-    {/* <div style={{fontFamily : 'YourFontThin'}}> */}
+    {/* <div className={YourFont.className}> */}
+    <div style={{fontFamily : 'YourFontThin'}}>
     <Navbar params = "Home"/>
     </div>
-    <div className={`bg-[#FFFAF5]`}>
-    {/* <div style={{fontFamily : 'YourFont'}} className={`bg-[#FFFAF5] `}> */}
+    {/* <div className={`bg-[#FFFAF5]`}> */}
+    <div style={{fontFamily : 'YourFont'}} className={`bg-[#FFFAF5] `}>
     <HeroSection/>
     </div>
     </>
