acc70e4ad19a3c33e5ff1f8e82789df720069c23
Shivesh Nandan <kshivesh929@gmail.com>
2025-04-23 00:36:46 +0530

    changes for loading page after login

--- my-app/src/app/Login/page.tsx
+++ my-app/src/app/Login/page.tsx
@@ -248,11 +248,12 @@ const LoginPage = () => {
                     </button>
 
                     {Btn ? 
-                    <div className={`w-full h-[100vh] bg-[#2d2d2da5] fixed left-0 top-0 z-[20000] justify-center`}>
-                    <div className="h-full flex">
-                    <Image src={'/loading.gif'} alt="ss" height={0} width={70} className=' m-auto '/>
-                    </div>
-                  </div>
+                  //   <div className={`w-full h-[100vh] bg-[#2d2d2da5] fixed left-0 top-0 z-[20000] justify-center`}>
+                  //   <div className="h-full flex">
+                  //   <Image src={'/loading.gif'} alt="ss" height={0} width={70} className=' m-auto '/>
+                  //   </div>
+                  // </div>
+                  <Loading/>
                   : 
                   <p></p> }
                     
